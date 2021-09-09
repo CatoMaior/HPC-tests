3531fff2590ac25ff63d9fe27bebad04ea88b868
catomaior <pietro.firpo@pm.me>
2021-09-09 21:20:23 +0200

    Moved a cudaMemCpy and changed some formatting

--- isingCuda.cu
+++ isingCuda.cu
@@ -25,19 +25,19 @@ void randomizeS() {
             S[i][j] = rand() > RAND_MAX / 2 ? 1 : -1;
 }
 
-__device__ float generate(curandState* globalState, int ind) {
+__device__ float generate(curandState *globalState, int ind) {
     curandState localState = globalState[ind];
-    float RANDOM = curand_uniform(&localState);
+    float randNum = curand_uniform(&localState);
     globalState[ind] = localState;
-    return RANDOM;
+    return randNum;
 }
 
-__global__ void setup_kernel(curandState * state, unsigned long seed) {
+__global__ void setup_kernel(curandState *state, unsigned long seed) {
     int id = threadIdx.x;
     curand_init(seed, id, 0, &state[id]);
 }
 
-__global__ void updateBoard(char* gpuS, float* T, curandState *states) {
+__global__ void updateBoard(char *gpuS, float *T, curandState *states) {
     int id = threadIdx.x;
     int x = ((int) (generate(states, id) * 1000000)) % (N - 1);
     int y = ((int) (generate(states, id) * 1000000)) % (N - 1);
@@ -46,28 +46,25 @@ __global__ void updateBoard(char* gpuS, float* T, curandState *states) {
                                     *(gpuS + ((x + 1) % N) * N + (y - 1) % N) +
                                     *(gpuS + ((x - 1) % N) * N + (y + 1) % N) +
                                     *(gpuS + ((x - 1) % N) * N + (y - 1) % N)) -
-                                    2 * H * *(gpuS + N*x + y);
+                                    2 * H * *(gpuS + N * x + y);
     __syncthreads();
     if (deltaE < 0 || exp((float) - deltaE / (Kb * *T)) > generate(states, id))
         *(gpuS + N * x + y) *= -1;
 }
 
-float *runCycles(float T, curandState* devStates) {
+float *runCycles(float T, curandState *devStates, char *gpuS) {
     float *magnArr = (float *)malloc(N_SAMPLES * sizeof(float));
     float *energyArr = (float *)malloc(N_SAMPLES * sizeof(float));
     float magn, energy;
     int insertedSamples = 0;
-    
-    char* gpuS;
+
     float* Ts;
-    cudaMalloc((void **) &gpuS, N * N * sizeof(char));
     cudaMalloc((void **) &Ts, sizeof(float));
     cudaMemcpy(Ts, S, sizeof(float), cudaMemcpyHostToDevice);
 
     for (unsigned int i = 0; i < TRIGGER + N_SAMPLES; i++) {
         magn = 0;
         energy = 0;
-        cudaMemcpy(gpuS, S, N * N, cudaMemcpyHostToDevice);
         for (unsigned int j = 0; j < SAMPLE_DELAY; j++) {
             updateBoard<<<1, N_THREAD>>>(gpuS, Ts, devStates);
             cudaDeviceSynchronize();
@@ -78,7 +75,7 @@ float *runCycles(float T, curandState* devStates) {
                 for (int k = 0; k < N; k++)
                     magn += S[j][k];
             }
-            magnArr[insertedSamples] = (float)magn / (N * N);
+            magnArr[insertedSamples] = magn / (N * N);
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++)
                     energy += J * S[j][k] * (S[(j + 1) % N][k] + S[j][(k + 1) % N]) - 2 * H * S[j][k];
@@ -87,6 +84,8 @@ float *runCycles(float T, curandState* devStates) {
             insertedSamples++;
         }
     }
+
+    cudaFree(Ts);
 
     float susc = 0, sq_av = 0, av_sq = 0, cal = 0;
     magn = 0;
@@ -127,21 +126,25 @@ float *runCycles(float T, curandState* devStates) {
 int main() {
     srand(time(NULL));
     randomizeS();
+    char* gpuS;
+    cudaMalloc((void **) &gpuS, N * N * sizeof(char));
+    cudaMemcpy(gpuS, S, N * N, cudaMemcpyHostToDevice);
     curandState* devStates;
     cudaMalloc(&devStates, N * sizeof(curandState));
     int seed = rand();
-    setup_kernel<<<1, N_THREAD>>>(devStates,seed);
+    setup_kernel<<<1, N_THREAD>>>(devStates, seed);
     cudaDeviceSynchronize();
     float *resArr;
     float susc[NUM_STEP], temp[NUM_STEP], heat[NUM_STEP], arrMagn[NUM_STEP];
     for (int i = 0; i < NUM_STEP; i++) {
         float t = MIN_TEMP + (MAX_TEMP - MIN_TEMP) / NUM_STEP * i;
-        resArr = runCycles(t, devStates);
+        resArr = runCycles(t, devStates, gpuS);
         temp[i] = resArr[0];
         susc[i] = resArr[1];
         heat[i] = resArr[2];
         arrMagn[i] = resArr[3];
         free(resArr);
-    }    
+    }
+    cudaFree(gpuS);
     return 0;
 }
